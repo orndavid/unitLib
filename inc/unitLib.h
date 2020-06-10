5ae0952174878b86064418ee144d01386bd811d0
orndavid <orn.david@gmail.com>
2020-06-11 08:51:07 +1000

    Starting files, header and production code

---
+++ inc/unitLib.h
@@ -0,0 +1,14 @@
+/** unitLib.h
+   Unit conversion library
+   Author: David Orn, orn.david@gmail.com
+   Create a library that types various units and allows for various unit
+   conversions.
+ */
+#ifndef _UNITLIB_H__
+#define _UNITLIB_H__
+
+
+
+
+
+#endif //_UNITLIB_H__
---
+++ src/unitLib.cpp
@@ -0,0 +1,1 @@
+#include <unitLib.h>
