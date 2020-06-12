92516bcff0c09d49cf866e243084cef725004c61
orndavid <orn.david@gmail.com>
2020-06-12 14:17:08 +1000

    Update on the frontend, got the standard print to work, trying to figure out the +operator

--- inc/unitLib.h
+++ inc/unitLib.h
@@ -1,20 +1,51 @@
 /** unitLib.h
-   Unit conversion library
+   Unit conversion library API
    Author: David Orn, orn.david@gmail.com
-   Create a library that types various units and allows for various unit
-   conversions.
- */
+
+   This takes care of unit conversion from one to another and, further, ensure unit type
+   to be strong
+*/
 #ifndef _UNITLIB_H__
 #define _UNITLIB_H__
+#include <unitLib_templates.h>
+#include <string>
 
-
-#include <iostream>
 
 template<class T>
-T match(T value){
-  return value;
- }
+class Kg : public Base<T>{
+private:
+  std::string unit_{"kg"};
+public:
+  Kg(): Base<T>(){};
+  Kg(T& x) : Base<T>(x){};
+  Kg(T&& x): Base<T>(x){};
+  ~Kg(){};
+  std::string unit(){return unit_;};
+};
 
+template<class T>
+class lbs : public Base<T>{
+private:
+  const std::string unit_{"lbs"};
+public:
+  lbs(): Base<T>(){};
+  lbs(T& x) : Base<T>(x){};
+  lbs(T&& x): Base<T>(x){};
+  ~lbs(){};
 
+  std::string unit(){return unit_;};
+  };
+
+template<class T>
+class Force : public Base<T>{
+private:
+  const std::string unit_{"F"};
+public:
+  Force(): Base<T>(){};
+  Force(double& x): Base<T>(x){};
+  Force(double&& x): Base<T>(x){};
+  ~Force(){};
+
+};
 
 #endif //_UNITLIB_H__
